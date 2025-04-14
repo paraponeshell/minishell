5b3252f7586dbf237e59acb843d7cdce2b48f9a8
Julien Meli <jmeli@c1r6s1.42luxembourg.lu>
2025-04-14 16:13:21 +0200

    norminette

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -3,19 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aharder <aharder@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jmeli <jmeli@student.42luxembourg.lu>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 18:25:45 by aharder           #+#    #+#             */
-/*   Updated: 2025/04/14 12:54:24 by aharder          ###   ########.fr       */
+/*   Updated: 2025/04/14 16:11:43 by jmeli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+int	display_error(char *arg)
+{
+	ft_putstr_fd("env: \'", 2);
+	ft_putstr_fd(cmd[i], 2);
+	ft_putendl_fd("\': No such file or directory", 2);
+	return (1);
+}
+
 int	env(t_env **env, char **cmd)
 {
 	t_env	*ptr;
-	int	i;
+	int		i;
 
 	ptr = *env;
 	i = 0;
@@ -23,10 +31,7 @@ int	env(t_env **env, char **cmd)
 	{
 		if (ft_strcmp(cmd[i], "env") != 0)
 		{
-			ft_putstr_fd("env: \'", 2);
-			ft_putstr_fd(cmd[i], 2);
-			ft_putendl_fd("\': No such file or directory", 2);
-			exit(1);
+			exit(display_error(cmd[i]));
 		}
 		i++;
 	}
@@ -35,7 +40,8 @@ int	env(t_env **env, char **cmd)
 		if (ptr->result)
 		{
 			if ((strcmp(ptr->value, "?") != 0) && (strcmp(ptr->value, "&") != 0)
-			&& (strcmp(ptr->value, "+") != 0) && (strcmp(ptr->result, "''") != 0))
+				&& (strcmp(ptr->value, "+") != 0) && (strcmp(ptr->result,
+						"''") != 0))
 				printf("%s=%s\n", ptr->value, ptr->result);
 		}
 		ptr = ptr->next;
