a43c11b957be180e82baafe1957fa37af1eba927
Julien Meli <jmeli@c1r6s1.42luxembourg.lu>
2025-04-14 17:45:03 +0200

    norminette

--- src/builtin/export_alphanum_utils.c
+++ src/builtin/export_alphanum_utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export_alphanum_utils.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmeli <jmeli@student.42luxembourg.lu>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 17:43:27 by jmeli             #+#    #+#             */
+/*   Updated: 2025/04/14 17:44:36 by jmeli            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/minishell.h"
 
 int	arg_val_has_valid_chars(char *arg)
@@ -25,10 +37,9 @@ int	arg_var_has_valid_chars(char *arg)
 	int	i;
 
 	i = 0;
-	
 	if (arg[0] && (arg[0] == '=' || ft_isdigit(arg[0])))
 		return (0);
-	while (arg[i] && arg[i]!= '=')
+	while (arg[i] && arg[i] != '=')
 	{
 		if (!ft_isalnum(arg[i]) && arg[i] != '_')
 		{
@@ -41,30 +52,3 @@ int	arg_var_has_valid_chars(char *arg)
 	}
 	return (1);
 }
-
-/*
-int	unset_arg_var_has_valid_chars(char *arg)
-{
-	int	i;
-	int	res;
-
-	i = 0;
-	res = 0;
-	if (arg[0] && (arg[0] == '=' || ft_isdigit(arg[0])))
-		return (0);
-	while (arg[i] && arg[i]!= '=')
-	{
-		if (!ft_isalnum(arg[i]) && arg[i] != '_')
-		{
-			if (arg[i] == '+' && (!arg[i + 1] || arg[i + 1] == '='))
-				return (1);
-			else
-				return (0);
-		}
-		i++;
-	}
-	if (arg[i] == '=' && arg[i + 1] == '\0')
-		res = 0;
-	return (res);
-}
-*/
